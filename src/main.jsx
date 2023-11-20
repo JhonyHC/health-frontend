d374365744041ebd3e1fd94cf1e9ffc07adbc620
JhonyHC <jonhatan.higuera5180@alumnos.udg.mx>
2023-11-20 13:48:31 -0600

    Add MUI dependencies and import SignIn component

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import SignIn from './SignIn.jsx';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <SignIn />
   </React.StrictMode>,
 )
