95971ad31b6588d0790201361eb46d353d89eb73
JhonyHC <jonhatan.higuera5180@alumnos.udg.mx>
2023-12-01 16:09:01 -0600

    Cambia Dieta form a formik y agrega Joi

--- src/components/Dieta.jsx
+++ src/components/Dieta.jsx
@@ -5,7 +5,6 @@ import {
   Button,
   Card,
   CardContent,
-  TextField,
   List,
   ListItem,
   Typography,
@@ -15,6 +14,8 @@ import {
   DialogActions,
 } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
+import { Formik, Form, Field } from 'formik';
+import { TextField } from 'formik-mui';
 
 const initialEntry = {
   nombre: '',
@@ -56,22 +57,11 @@ const initialData = [
 const DietList = () => {
   const [entries, setEntries] = useState([]);
   const [openDialog, setOpenDialog] = useState(false);
-  const [newEntry, setNewEntry] = useState(initialEntry);
 
   useEffect(() => {
     // Mostrar datos iniciales al cargar el componente
     setEntries(initialData);
   }, []);
-
-  const handleInputChange = (field, value) => {
-    setNewEntry({ ...newEntry, [field]: value });
-  };
-
-  const handleAddEntry = () => {
-    setEntries([...entries, newEntry]);
-    setNewEntry(initialEntry);
-    setOpenDialog(false);
-  };
 
   return (
     <div>
@@ -112,66 +102,111 @@ const DietList = () => {
       <Dialog open={openDialog} onClose={() => setOpenDialog(false)}>
         <DialogTitle>Agregar Entrada de Dieta</DialogTitle>
         <DialogContent>
-          <TextField
-            label="Nombre"
-            fullWidth
-            value={newEntry.nombre}
-            onChange={(e) => handleInputChange('nombre', e.target.value)}
-          />
-          <TextField
-            label="Descripción"
-            fullWidth
-            value={newEntry.descripcion}
-            onChange={(e) => handleInputChange('descripcion', e.target.value)}
-          />
-          <TextField
-            label="Tipo de Dieta"
-            fullWidth
-            value={newEntry.tipoDieta}
-            onChange={(e) => handleInputChange('tipoDieta', e.target.value)}
-          />
-          <TextField
-            label="Calorías"
-            fullWidth
-            value={newEntry.calorias}
-            onChange={(e) => handleInputChange('calorias', e.target.value)}
-          />
-          <TextField
-            label="Proteínas"
-            fullWidth
-            value={newEntry.proteinas}
-            onChange={(e) => handleInputChange('proteinas', e.target.value)}
-          />
-          <TextField
-            label="Carbohidratos"
-            fullWidth
-            value={newEntry.carbohidratos}
-            onChange={(e) => handleInputChange('carbohidratos', e.target.value)}
-          />
-          <TextField
-            label="Grasas"
-            fullWidth
-            value={newEntry.grasas}
-            onChange={(e) => handleInputChange('grasas', e.target.value)}
-          />
-          <TextField
-            label="Fibra"
-            fullWidth
-            value={newEntry.fibra}
-            onChange={(e) => handleInputChange('fibra', e.target.value)}
-          />
-          <TextField
-            label="Imagen (URL)"
-            fullWidth
-            value={newEntry.imagen}
-            onChange={(e) => handleInputChange('imagen', e.target.value)}
-          />
+          <Formik
+            initialValues={{
+              nombre: '',
+              descripcion: '',
+              tipoDieta: '',
+              calorias: '',
+              proteinas: '',
+              carbohidratos: '',
+              grasas: '',
+              fibra: '',
+              imagen: '',
+            }}
+            validate={(values) => {
+              const errors = {};
+
+              return errors;
+            }}
+            onSubmit={(values, { setSubmitting }) => {
+              setTimeout(() => {
+                setSubmitting(false);
+                alert(JSON.stringify(values, null, 2));
+                setEntries([...entries, values]);
+                setOpenDialog(false);
+              }, 500);
+            }}
+          >
+            {({ submitForm, isSubmitting }) => (
+              <Form>
+                <Field
+                  component={TextField}
+                  name="nombre"
+                  label="Nombre"
+                  type="text"
+                  fullWidth
+                />
+                <Field
+                  component={TextField}
+                  name="descripcion"
+                  label="Descripción"
+                  type="text"
+                  fullWidth
+                />
+                <Field
+                  component={TextField}
+                  name="tipoDieta"
+                  label="Tipo de Dieta"
+                  type="text"
+                  fullWidth
+                />
+                <Field
+                  component={TextField}
+                  name="calorias"
+                  label="Calorías"
+                  type="number"
+                  fullWidth
+                />
+                <Field
+                  component={TextField}
+                  name="proteinas"
+                  label="Proteínas"
+                  type="number"
+                  fullWidth
+                />
+                <Field
+                  component={TextField}
+                  label="Carbohidratos"
+                  name="carbohidratos"
+                  type="number"
+                  fullWidth
+                />
+                <Field
+                  component={TextField}
+                  name="grasas"
+                  label="Grasas"
+                  type="number"
+                  fullWidth
+                />
+                <Field
+                  component={TextField}
+                  name="fibra"
+                  label="Fibra"
+                  type="number"
+                  fullWidth
+                />
+                <Field
+                  component={TextField}
+                  name="url"
+                  label="Imagen (URL)"
+                  type="url"
+                  fullWidth
+                />
+                <Button variant="contained"
+                  color="primary"
+                  disabled={isSubmitting}
+                  onClick={submitForm}>Agregar</Button>
+              </Form>
+            )}
+          </Formik>
+
         </DialogContent>
         <DialogActions>
           <Button onClick={() => setOpenDialog(false)}>Cancelar</Button>
-          <Button onClick={handleAddEntry} variant="contained" color="primary">
+          {/* <Button onClick={handleAddEntry} variant="contained" color="primary">
             Agregar
-          </Button>
+          </Button> */}
         </DialogActions>
       </Dialog>
     </div>
